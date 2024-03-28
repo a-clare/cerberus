42fdc7ff7a98c7db9cdd92e6ab508884f3c4235f
Aclare <adam.jarrett.clare@gmail.com>
2024-03-27 20:26:37 -0600

    Cleaning up main and removing image window files

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 #include <cstdio>
 #include "kitti_data_directory.h"
 #include "logger.h"
-#include "image_window.h"
-#include "vis/vis.h"
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -14,7 +12,6 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  // vis_init("Cerberus", 1200, 1080);
   const char *input_dir = argv[1];
   LOG("Input directory is %s", input_dir);
 
@@ -29,15 +26,6 @@ int main(int argc, char *argv[]) {
   ImageU8Ptr right_image;
   kiti_data_dir_load_img_u8(ptr, KittiDataDirSensorNames::RightImgGrey, 0, &right_image);
 
-  // ImageWindowPtr left_window = image_window_create(left_image);
-  // ImageWindowPtr right_window = image_window_create(right_image);
-  // while (vis_is_window_running()) {
-  //   vis_frame_start();
-
-  //   image_window_draw(left_window, "left");
-  //   image_window_draw(right_window, "right");
-  //   vis_frame_end();
-  // }
   image_write(left_image);
   return 0;
 }
